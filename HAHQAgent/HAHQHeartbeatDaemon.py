dbec7876f01a1a4a67561e5f98db01bade877bb4
Jonas Depoix <jdepoix@seibert-media.net>
2016-01-24 21:22:08 +0100

    circular import fixed

--- HAHQAgent/HAHQHeartbeatDaemon.py
+++ HAHQAgent/HAHQHeartbeatDaemon.py
@@ -1,14 +1,14 @@
 from threading import Thread
 import time
 
-from HAHQConfiguredAgentInstance import HAHQConfiguredAgentInstance
+import HAHQConfiguredAgentInstance
 
 
 class HAHQHeartbeatDaemon(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.setDaemon(True)
-        self.agent = HAHQConfiguredAgentInstance()
+        self.agent = HAHQConfiguredAgentInstance.HAHQConfiguredAgentInstance()
 
     def run(self):
         while(True):
