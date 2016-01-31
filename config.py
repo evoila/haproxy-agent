a6479e857038ae6ee3aaf7af218b531524d0cd99
Jonas Depoix <jdepoix@seibert-media.net>
2016-01-31 14:23:58 +0100

    default config.py changed

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # the url and port of the server the HAProxyHQ is running on and the API endpoint for the config
-SERVER_ADDRESS = ''
-SERVER_PORT = '80'
-SERVER_API_ENDPOINT = 'config'
+SERVER_ADDRESS = 'http://my.haproxyhq.backend'
+SERVER_PORT = '8080'
+SERVER_API_ENDPOINT = 'agents'
 
 # the ID of this agent and it's token, which the HAProxyHQ will need to identify and authenticate this agent
 AGENT_ID = ''
 AGENT_TOKEN = ''
 
 # the adress and port of the MQTT broker
-MQTT_BROKER_ADRESS = ''
+MQTT_BROKER_ADRESS = 'my.mqtt.broker'
 MQTT_BROKER_PORT = '1883'
 
 # the path of the HAProxy config, which the agent will manage
-HA_PROXY_CONFIG_PATH = ''
+HA_PROXY_CONFIG_PATH = '/etc/haproxy/haproxy.cfg'
 
 # the MQTT topic the agent will subscribe to. There should be no need to change this!
 MQTT_TOPIC = '/haproxyhq/agents/' + AGENT_ID
